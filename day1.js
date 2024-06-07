9bfb9d4bd94e00f52ac469ebc5cf17f722981365
vaishnavibb12 <vaishabiramane@gmail.com>
2024-06-07 15:19:11 +0530

    first commit

---
+++ day1.js
@@ -0,0 +1,25 @@
+// console.log("hello world");
+
+// function greet() {
+//   console.log("Hello world");
+// }
+
+// setTimeout(greet, 3000);
+
+// var x = 2;
+// var y = 3;
+// var z = x + y;
+// console.log(z);
+
+// const cars = ["Maruti", "Honda"];
+// cars[0] = "Toyota";
+// cars.push("Audi");
+// console.log(cars);
+
+// let x = 10;
+// console.log(x);
+
+// let x = 2;
+// let y = 3;
+// let z = x * y;
+// console.log(z);
