8f1e64b9a18d7774990feab6c41191099068247c
vaishnavibb12 <vaishabiramane@gmail.com>
2024-06-07 18:21:55 +0530

    learing objects

--- day1.js
+++ day1.js
@@ -23,3 +23,75 @@
 // let y = 3;
 // let z = x * y;
 // console.log(z);
+
+// let x = 10;
+// x *= 5;
+// console.log(x);
+
+// Numbers:
+// let length = 16;
+// let weight = 7.5;
+
+// Strings:
+// let color = "Yellow";
+// let lastName = "Johnson";
+
+// Booleans
+// let x = true;
+// let y = false;
+
+// Object:
+// const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+// const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+// const date = new Date("2022-03-25");
+// console.log(date);
+
+// function myFunction(p1, p2) {
+//   return p1 + p2;
+// }
+
+// let result = myFunction(4, 3);
+// console.log(result);
+
+// function toCelsius(f, n) {
+//   return f - n;
+// }
+// let value = toCelsius(66, 32);
+// console.log(value);
+
+// const car2 = { type: "flat", model: "400", color: "white" };
+
+// console.log(`the car type is ${car2.type}, ${car2.model}, and ${car2.color} `);
+
+// const person = {
+//   firstname: "John",
+//   lastname: "Doe",
+//   age: 50,
+//   eyecolor: "blue",
+// };
+// person.nationality = "English";
+// console.log(`${person.firstname}, ${person.lastname} is ${person.nationality}`);
+
+// const person1 = { firstname: "vaishnavi", lastname: "biramane" };
+// const person2 = { firstname: "Anne", lastname: "Smith" };
+// const df = Object.assign(person1, person2);
+// console.log(df);
+
+// const fruits = { Bananas: 300, Oranges: 200, Apples: 500 };
+// const sd = fruits.entries(fruits);
+// console.log(sd);
+
+// const person = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   id: 5566,
+//   fullName: function () {
+//     return this.firstName + " " + this.lastName;
+//   },
+// };
+// let eeee = person.fullName();
+// console.log(eeee);
